93ea87d675f5643cb1e19c6b974ce635ce3171f1
Evan Peterson <evanpeterson17@gmail.com>
2021-10-21 21:38:04 -0600

    Improve documentation

--- hf_serving/__main__.py
+++ hf_serving/__main__.py
@@ -8,14 +8,16 @@ pipe = TypedPipeline(task=TASK, model=MODEL, tokenizer=TOKENIZER)
 app = FastAPI(
     title=f"{TASK.capitalize()} Service",
     description=(
-        f"A web service serving up prediction requests for the {TASK} task. See the [docs]({pipe.spec.docs_link}) "
-        "for more information about how the pipeline works."
+        f"A web service serving up prediction requests for the `{TASK}` task. If you're interested in the specifics of"
+        f"the pipeline and how it works, see the [docs]({pipe.spec.docs_link}). If you just want to consume the model,"
+        "see the API docs below for the `/predict` endpoint."
     )
 )
 
 
 @app.post("/predict", response_model=pipe.spec.output_model)
 def predict(inputs: pipe.spec.input_model):
+    """Call to make a prediction request to the ML pipeline."""
     return pipe(inputs)
 
 
