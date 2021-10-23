93ee7b41d03161c69983683694ff87b263a99fea
Evan Peterson <evanpeterson17@gmail.com>
2021-10-23 13:54:31 -0600

    Fix build script

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 set -eo pipefail
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "$DIR/.."  # go to root
 
 docker build --tag epeters3/hf-serving:latest --file Dockerfile .
